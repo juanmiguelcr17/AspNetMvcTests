16857174a0d43d0d03c9e0fec6ebc066089a4e6e
juanmiguelcr17 <jumicarod_93@hotmail.com>
2017-08-18 18:00:21 -0500

    Se añade proyecto para ejemplificar uso de Ajax con respuesta en Json y mostrar resultado en popup boostrap.

---
+++ AjaxHelpers/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AjaxHelpers.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AjaxCall()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AjaxCalling()
+        {
+            return Json(new { status = "success", data = new { Message = "Exito" } }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ AjaxHelpers/Scripts/Ajax.Validations.js
@@ -0,0 +1,19 @@
+﻿function OnSuccess(data) {
+    var json = $.parseJSON(data.responseText);
+    var data = json.data;
+    BootstrapDialog.show({
+        type: BootstrapDialog.TYPE_INFO,
+        title: 'Info',
+        message: data.Message
+    });
+}
+
+function OnError(data) {
+    var json = $.parseJSON(data.responseText);
+    var data = json.data;
+    BootstrapDialog.show({
+        type: BootstrapDialog.TYPE_DANGER,
+        title: 'Atención',
+        message: data.Message
+    });
+}
